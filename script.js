2a3fa4b92450f94e7a3dd1e5619b58b975129d3d
Arpit Mallick <arpit@Arpits-MacBook-Air.local>
2023-10-19 16:01:56 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-// undefined
+console.log(true, false, 'true', 'false');
 
-let age;
-console.log(age, age+3, `I am ${age} years old`);
+// methods that return boolean values
+let email = 'arpitmallick99@gmail.com';
+// console.log(email.includes('g'));
 
-// null
+let arr1 = ['Rahul', 'Raj', 20, 40];
+// console.log(arr1.includes('Raj'));
 
-let myAge = null;
-console.log(myAge, myAge+3, `I am ${myAge} years old`); 
+// comparison operators
+let age = 45;
 
-// Output for the null part is: 
-// null 3 'I am null years old'
+// console.log(age == 45);
+// console.log(age != 45);
+// console.log(age > 30);
+// console.log(age < 20);
+// console.log(age >= 45);
+// console.log(age <= 55);
+
+// let nm = 'john';
+// console.log(nm == 'john');
+// console.log(nm < 'shahid');
+// console.log(nm > 'John'); // LowerCase letter 'j' is greater than the upperCase letter 'J' i.e why false
+
+// loose comparison (Different types can still be equal)
+
+age = 25;
+console.log(age == 25);
+console.log(age == '25'); // The dataTypes doesn't matter here
+
+// strict comparison (Different types can't be equal)
+console.log(age === 25);
+console.log(age === '25'); // The dataTypes does matter here
+
+
+
+
+
