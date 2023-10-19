8b32c3ddf3880ab1faa459344fbba5676f06f47a
Arpit Mallick <arpit@Arpits-MacBook-Air.local>
2023-10-19 11:51:12 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
-// In arrays in JS, we can store values of different datatypes like lists in python
+// undefined
 
-let arr1 = ['Rahul', 'Rajiv', 'Raj', 'Arpit', 20, 30];
-console.log(arr1);
+let age;
+console.log(age, age+3, `I am ${age} years old`);
 
-console.log(arr1[2]);
+// null
 
-// array methods and properties
+let myAge = null;
+console.log(myAge, myAge+3, `I am ${myAge} years old`); 
 
-console.log(arr1.length);
-// join method:- joins the array elements with the specified characters
-
-// let res = arr1.join(", ");
-// console.log(res);
-
-// let res = arr1.indexOf(20);
-// console.log(res);
-
-// let res = arr1.concat([2,4,'John','Jimmy']); // Concatenates two arrays
-// console.log(res);
-
-let res = arr1.push('Kolkata'); // This method alters the original array with the new value
-// console.log(res);// Returns the new length of the array
-// console.log(arr1);
-res = arr1.pop(); // Deletes the last element in the array
-console.log(res); // Prints the element that is deleted from the array
-console.log(arr1);
-
-
-
-
+// Output for the null part is: 
+// null 3 'I am null years old'
