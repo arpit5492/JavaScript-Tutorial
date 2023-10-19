162bfb6fbb047b6c4e31c4b47f4df89157cd46d2
Arpit Mallick <arpit@Arpits-MacBook-Air.local>
2023-10-19 11:34:08 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-// template strings
-let book = "Oliver Twist";
-let author = "Charles Dickens";
-likes = 100;
+// In arrays in JS, we can store values of different datatypes like lists in python
 
-// concatenate way
-// let r = "The book named " + book + " by the author " + author + " has " + likes + " likes.";
-// console.log(r);
+let arr1 = ['Rahul', 'Rajiv', 'Raj', 'Arpit', 20, 30];
+console.log(arr1);
 
-// template strings way:- Easy way to concatenate
-// let res = `The book named ${book} written by the author ${author} has ${likes} likes.`;
+console.log(arr1[2]);
+
+// array methods and properties
+
+console.log(arr1.length);
+// join method:- joins the array elements with the specified characters
+
+// let res = arr1.join(", ");
 // console.log(res);
 
-// creating html template using template strings
-let html = `
-    <h2>${book}</h2>
-    <p>${author}</p>
-    <span>This book has ${likes} likes</span>
-`;
-console.log(html);
+// let res = arr1.indexOf(20);
+// console.log(res);
+
+// let res = arr1.concat([2,4,'John','Jimmy']); // Concatenates two arrays
+// console.log(res);
+
+let res = arr1.push('Kolkata'); // This method alters the original array with the new value
+// console.log(res);// Returns the new length of the array
+// console.log(arr1);
+res = arr1.pop(); // Deletes the last element in the array
+console.log(res); // Prints the element that is deleted from the array
+console.log(arr1);
+
+
+
+
