0b4d684af8738e03c5857c2c4d2ddfd8db4c8704
Arpit Mallick <arpit@Arpits-MacBook-Air.local>
2023-10-19 16:36:42 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-let score = '100';
+// for(let i=0;i<5;i++){
+//     console.log(i);
+// }
+// console.log('Loop finished');
 
-// score = Number(score);
-// console.log(typeof(score), score+1);
-
-let naam = 'Arpit';
-console.log(Number(naam));
-let sc = 30;
-console.log(String(sc));
-let result = Boolean(0); // Only the boolean of 0 is false otherwise it's always true
-console.log(typeof(result), result);
-
-let res = Boolean('0');
-console.log(res);
-
-res = Boolean(14);
-console.log(res);
-
-
-
-
-
+const names = ['Arpit', 'Sahil', 'Priyanshu', 'Anirudh'];
+for(let i=0;i<names.length;i++){
+    let html = `<div>${names[i]}</div>`;
+    console.log(html);
+}
