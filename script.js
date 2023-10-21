3e8c4cb65903b25aef382d12650101958bfb5df0
Arpit Mallick <arpit@Arpits-Air.lan>
2023-10-20 23:19:43 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// for(let i=0;i<5;i++){
-//     console.log(i);
+// const names = ['Arpit', 'Sahil', 'Priyanshu', 'Anirudh'];
+// let i = 0;
+// while(i<names.length){
+//     console.log(names[i]);
+//     i++;
 // }
-// console.log('Loop finished');
 
-const names = ['Arpit', 'Sahil', 'Priyanshu', 'Anirudh'];
-for(let i=0;i<names.length;i++){
-    let html = `<div>${names[i]}</div>`;
-    console.log(html);
-}
+let i = 4;
+do{
+    console.log(i);
+    i++;
+}while(i<5);
