d75aecae7be4b763e5dce9c9a80b47fccd1be558
Arpit Mallick <arpit@Arpits-Air.lan>
2023-10-21 01:02:26 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-// break and continue statement
+// Switch statement follows strich equality(===)
 
-const scores = [25, 30, 0, 86, 100, 20, 5];
-for(let i = 0; i < scores.length; i++){
-    if(scores[i] === 0){
-        continue;
-    }
-    console.log("Your score is: ", scores[i]);
-    if(scores[i] === 100){
-        console.log("Congratulations!! You have reached the top score");
+const grade = 'A'
+switch(grade){
+    case 'A':
+        console.log("You have got a A grade");
         break;
-    }
+    case 'B':
+        console.log("You have got a B grade");
+        break;
+    case 'C':
+        console.log("You have got a C grade");
+        break;
+    case 'D':
+        console.log("You have got a D grade");
+        break;
+    case 'F':
+        console.log("You have got a F grade");
+        break;
+    default:
+        console.log("Invalid grade");
 }
