5dfbfaabaa405182a3096eea5dfaaace6f4b852b
Arpit Mallick <arpit@Arpits-Air.lan>
2023-10-21 18:13:20 -0700

    Initial commit

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-// returning values
-const calArea = function(radius){
-    return 3.14 * radius**2;
-};
+// arrow function
+// const calcArea = (radius) => {
+//     return 3.14 * radius**2;
+// };
 
-const area = calArea(5);
+// More shorter way to write an arrow function
+const calcArea = radius => 3.14 * radius**2;
+const area = calcArea(5);
 console.log("Area is: ", area);
+
+const greet = () => 'Hello World!!';
+console.log(greet());
+
+const bill = (products, tax) => {
+    let total = 0;
+    for(let i = 0; i<products.length;i++){
+        total+=products[i]+products[i]*tax;
+    }
+    return total;
+}
+
+console.log(bill([23,45,21,67,32], 0.2));
