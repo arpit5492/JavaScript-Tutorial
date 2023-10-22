a67a7d6b10d7fbe711155b34a3814f04a762cafa
Arpit Mallick <arpit@Arpits-Air.lan>
2023-10-21 23:29:54 -0700

    Initial commit

--- script.js
+++ script.js
@@ -5,19 +5,35 @@ let user = {
     age: 30,
     email: 'arjun1967@gmail.com',
     location: 'Italy',
-    blogs: ['Why mac & cheese rules', '10 things to be on the top of everyone']
+    blogs: ['Why mac & cheese rules', '10 things to be on the top of everyone'],
+    // login: () => {
+    //     onsole.log("The user is logged in");
+    // },
+    login(){ // shorthand for the above
+        console.log("The user is logged in");
+    },
+    // logout: () => {
+    //     onsole.log("The user is logged in");
+    // },
+    logout(){
+        console.log("The user is logged out");
+    },
+    // logBlogs: function(){ // With this keyword, arrow function won't work
+    //     // console.log(this.blogs);
+    //     console.log("This blogger has written the following blogs:");
+    //     this.blogs.forEach(blog => {
+    //         console.log(blog);
+    //     });
+    // }
+    logBlogs(){ // With this keyword, arrow function won't work
+        // console.log(this.blogs);
+        console.log("This blogger has written the following blogs:");
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        });
+    }
 };
 
-console.log(user);
-console.log(user.name);
-
-console.log(user['email']);
-
-user.name = 'Rahul';
-console.log(user.name);
-
-user['age'] = 45;
-
-console.log(user.age);
-
-console.log(typeof(user));
+user.login();
+user.logout();
+user.logBlogs();        
